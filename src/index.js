1fa8d4bd22fe2f60b058af1c159b058f7802cbf3
Aurimas <aurimas.skaburskas@gmail.com>
2017-07-04 21:06:22 +0300

    Preparty

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// Set up your application entry point here...
+import 'babel-polyfill';
+import configureStore from './store/configureStore';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import routes from './routes';
+import './styles/styles.css'; //Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
+
+
+const store = configureStore();
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
+    document.getElementById('app')
+);
